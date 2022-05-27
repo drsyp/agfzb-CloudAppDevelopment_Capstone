4281b997f3eeb9fe7629640e9b101fe52def1462
Sylvain Perrenoud <sylvain.perrenoud@gmail.com>
2022-05-27 08:48:28 +0000

    not finished

--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -54,57 +54,53 @@ class CarModel(models.Model):
 
 # <HINT> Create a plain Python class `CarDealer` to hold dealer data
 class CarDealer:
-    def __init__(self):
-        self.state = None
-        self.zip = None
-        self.st = None
-        self.long = None
-        self.lat = None
-        self.id = None
-        self.city = None
-        self.address = None
 
-    def build_from(self, json_data):
-        self.address = json_data["address"]
-        self.city = json_data["city"]
-        self.id = json_data["id"]
-        self.lat = json_data["lat"]
-        self.long = json_data["long"]
-        self.st = json_data["st"]
-        self.zip = json_data["zip"]
-        self.state = json_data["state"]
-
-        return self
+    def __init__(self, address, city, full_name, id, lat, long, short_name, st, zip):
+        # Dealer address
+        self.address = address
+        # Dealer city
+        self.city = city
+        # Dealer Full Name
+        self.full_name = full_name
+        # Dealer id
+        self.id = id
+        # Location lat
+        self.lat = lat
+        # Location long
+        self.long = long
+        # Dealer short name
+        self.short_name = short_name
+        # Dealer state
+        self.st = st
+        # Dealer zip
+        self.zip = zip
 
     def __str__(self):
-        return "Dealer address: " + self.address
+        return "Dealer name: " + self.full_name
 
 # <HINT> Create a plain Python class `DealerReview` to hold review data
 class DealerReview:
-    def __init__(self):
-        self.dealership = None
-        self.name = None
-        self.purchase = None
-        self.review = None
-        self.purchase_date = None
-        self.car_make = None
-        self.car_model = None
-        self.car_year = None
-        self.sentiment = None
-        self.id = None
-
-    def build_from(self, json_data):
-        self.dealership = json_data["dealership"]
-        self.name = json_data["name"]
-        self.purchase = json_data["purchase"]
-        self.review = json_data["review"]
-        self.purchase_date = json_data["purchase_date"]
-        self.car_make = json_data["car_make"]
-        self.car_model = json_data["car_model"]
-        self.car_year = json_data["car_year"]
-        self.id = json_data["id"]
-        return self
-
-    def with_sentiment(self, sentiment):
+    def __init__(self, dealership, name, purchase, review, purchase_date, car_make, car_model, car_year, sentiment,id):
+        # Dealer dealership
+        self.dealership = dealership
+        # Dealer name
+        self.name = name
+        # Dealer purchase
+        self.purchase = purchase
+        # Dealer review
+        self.review = review
+        # purchase_date
+        self.purchase_date = purchase_date
+        # car_make
+        self.car_make = car_make
+        # car_model
+        self.car_model = car_model
+        # car_year
+        self.car_year = car_year
+        # sentiment
         self.sentiment = sentiment
-        return self
+        # id
+        self.id = id
+
+    def __str__(self):
+        return "Dealer name: " + self.full_name
